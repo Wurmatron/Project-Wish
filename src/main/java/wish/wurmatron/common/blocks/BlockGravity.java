f20a8545343a3322e275604e5175cde5164f7bba
Wurmatron <wurmatron@gmail.com>
2017-09-13 21:22:50 -0400

    Fixed Gravity in the most lazy way possible

--- src/main/java/wish/wurmatron/common/blocks/BlockGravity.java
+++ src/main/java/wish/wurmatron/common/blocks/BlockGravity.java
@@ -13,68 +13,78 @@ import java.util.Collections;
 import java.util.Random;
 
 /**
-	* Gives gravity to a block
-	*/
+ Gives gravity to a block
+ */
 public class BlockGravity extends BlockFalling {
 
-		public BlockGravity(Material m) {
-				super(m);
-		}
+	private static final Random rand = new Random ();
 
-		@Override
-		public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-				if (!world.isRemote) {
-						super.updateTick(world, pos, state, rand);
-						checkSides(world, pos, state);
-				}
-		}
+	public BlockGravity (Material m) {
+		super (m);
+	}
 
-		private void checkSides(World world, BlockPos pos, IBlockState state) {
-				if (!world.isRemote) {
-						BlockPos newLocation = checkSide(world, pos, false);
-						if (newLocation != null) {
-								world.setBlockState(newLocation, world.getBlockState(pos));
-								world.setBlockToAir(pos);
-						}
-				}
+	@Override
+	public void updateTick (World world,BlockPos pos,IBlockState state,Random rand) {
+		if (!world.isRemote) {
+			super.updateTick (world,pos,state,rand);
+			checkSides (world,pos,state);
 		}
+	}
 
-		private static BlockPos checkSide(World world, BlockPos pos, boolean downCheck) {
-				ArrayList<BlockPos> possibleLocations = new ArrayList<>();
-				if (isValidMove(world, pos.east()))
-					if (downCheck || isValidMove(world, pos.east().down()))
-						possibleLocations.add(pos.east().down());
-				if (isValidMove(world, pos.west()))
-						if(downCheck || isValidMove(world, pos.west().down()))
-							possibleLocations.add(pos.west().down());
-				if (isValidMove(world, pos.south()))
-						if(downCheck || isValidMove(world, pos.south().down()))
-						  possibleLocations.add(pos.south().down());
-				if (isValidMove(world, pos.north()))
-					if(downCheck || isValidMove(world, pos.north().down()))
-						possibleLocations.add(pos.north().down());
-				if (possibleLocations.size() > 0) {
-						Collections.shuffle(possibleLocations);
-						BlockPos location = possibleLocations.get(0);
-						if (!world.isAirBlock(location.up())) world.setBlockToAir(location.up());
-						if (!world.isAirBlock(location)) world.setBlockToAir(location);
-						return location;
-				}
-				return null;
+	private void checkSides (World world,BlockPos pos,IBlockState state) {
+		if (!world.isRemote) {
+			BlockPos newLocation = checkSide (world,pos,false);
+			if (newLocation != null) {
+				world.setBlockState (newLocation,world.getBlockState (pos));
+				world.setBlockToAir (pos);
+			}
 		}
+	}
 
-		private static boolean isValidMove(World world, BlockPos pos) {
-				if (world.isAirBlock(pos) || canFallThrough(world.getBlockState(pos))) return true;
-				return false;
+	private static BlockPos checkSide (World world,BlockPos pos,boolean downCheck) {
+		ArrayList <BlockPos> possibleLocations = new ArrayList <> ();
+		if (!(rand.nextInt (4) % 4 == 1)) {
+			if (isValidMove (world,pos.east ()))
+				if (downCheck || isValidMove (world,pos.east ().down ()))
+					possibleLocations.add (pos.east ().down ());
+			if (isValidMove (world,pos.west ()))
+				if (downCheck || isValidMove (world,pos.west ().down ()))
+					possibleLocations.add (pos.west ().down ());
+			if (isValidMove (world,pos.south ()))
+				if (downCheck || isValidMove (world,pos.south ().down ()))
+					possibleLocations.add (pos.south ().down ());
+			if (isValidMove (world,pos.north ()))
+				if (downCheck || isValidMove (world,pos.north ().down ()))
+					possibleLocations.add (pos.north ().down ());
+			if (possibleLocations.size () > 0) {
+				Collections.shuffle (possibleLocations);
+				BlockPos location = possibleLocations.get (0);
+				if (!world.isAirBlock (location.up ()))
+					world.setBlockToAir (location.up ());
+				if (!world.isAirBlock (location))
+					world.setBlockToAir (location);
+				return location;
+			}
 		}
+		if (isValidMove (world,pos.down ())) {
+			if (!world.isAirBlock (pos.down ()))
+				world.setBlockToAir (pos.down ());
+			return pos.down ();
+		}
+		return null;
+	}
 
-		public int tickRate(World worldIn) {
-				return 4;
-		}
+	private static boolean isValidMove (World world,BlockPos pos) {
+		return world.isAirBlock (pos) || canFallThrough (world.getBlockState (pos));
+	}
 
-		public static boolean canFallThrough(IBlockState state) {
-				Block    block    = state.getBlock();
-				Material material = state.getMaterial();
-				return block == Blocks.FIRE || material == Material.AIR || material == Material.WATER || material == Material.LAVA;
-		}
+	public int tickRate (World worldIn) {
+		return 2;
+	}
+
+	public static boolean canFallThrough (IBlockState state) {
+		Block block = state.getBlock ();
+		Material material = state.getMaterial ();
+		return block == Blocks.FIRE || material == Material.AIR || material == Material.WATER || material == Material.LAVA || material == Material.PLANTS || block == Blocks.TALLGRASS;
+	}
 }
--- src/main/java/wish/wurmatron/common/world/WishWorldType.java
+++ src/main/java/wish/wurmatron/common/world/WishWorldType.java
@@ -3,23 +3,24 @@ package wish.wurmatron.common.world;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.gen.ChunkProviderOverworld;
 
 /**
-	* Default (Wish) world type
-	*/
+ Default (Wish) world type
+ */
 public class WishWorldType extends WorldType {
 
-		public WishWorldType(String name) {
-				super(name);
-		}
+	public WishWorldType (String name) {
+		super (name);
+	}
 
-		@Override
-		public IChunkGenerator getChunkGenerator(World world, final String generatorOptions) {
-				return new WishChunkProvider(world);
-		}
+	@Override
+	public IChunkGenerator getChunkGenerator (World world,final String generatorOptions) {
+		return new ChunkProviderOverworld (world,world.rand.nextLong (),true,"");
+	}
 
-		@Override
-		public float getCloudHeight() {
-				return 255f;
-		}
+	@Override
+	public float getCloudHeight () {
+		return 255f;
+	}
 }
